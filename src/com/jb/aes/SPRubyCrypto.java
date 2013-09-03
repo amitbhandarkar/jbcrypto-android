012a0c5a1d322666f3d182a936a10c1694c8ab2d
kzer95 <jlorenzo@stratpoint.com>
2013-09-03 21:35:14 +0800

    Use base64

--- src/com/jb/aes/SPRubyCrypto.java
+++ src/com/jb/aes/SPRubyCrypto.java
@@ -20,24 +20,11 @@ import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.http.util.ByteArrayBuffer;
 
+import android.util.Base64;
+
 public class SPRubyCrypto {
 	
 	private static int AES128KeySizeInBytes = 16; 
-	
-	static byte[] makeKey(String seed) {
-		try {
-			MessageDigest md = MessageDigest.getInstance("PKCS5");
-			byte[] key = md.digest(seed.getBytes("UTF-8"));
-			return key;
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-
-		return null;
-	}
-
 	
 	public static String encrypt(String seed, String cleartext) throws Exception {
         byte[] rawKey = seed.getBytes("utf-8"); 
@@ -46,11 +33,11 @@ public class SPRubyCrypto {
         
         byte[] result = encrypt(rawKey, rawText);
         
-        return toHex(result);
+        return Base64.encodeToString(result, android.util.Base64.DEFAULT);
 	}
 	
 	public static String decrypt(String seed, String encrypted) throws Exception {
-	        byte[] enc = toByte(encrypted);
+	        byte[] enc = encrypted.getBytes();
 	        byte[] result = decrypt(getPaddedRawKey(seed), enc);
 	        return new String(result);
 	}
@@ -96,39 +83,10 @@ public class SPRubyCrypto {
 	
 	private static byte[] decrypt(byte[] raw, byte[] encrypted) throws Exception {
 	    SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-	    Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+	    Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
 	    cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
 	    byte[] decrypted = cipher.doFinal(encrypted);
 	    
 	    return decrypted;
-	}
-	
-	public static String toHex(String txt) {
-	        return toHex(txt.getBytes());
-	}
-	public static String fromHex(String hex) {
-	        return new String(toByte(hex));
-	}
-	
-	public static byte[] toByte(String hexString) {
-	        int len = hexString.length()/2;
-	        byte[] result = new byte[len];
-	        for (int i = 0; i < len; i++)
-	                result[i] = Integer.valueOf(hexString.substring(2*i, 2*i+2), 16).byteValue();
-	        return result;
-	}
-	
-	public static String toHex(byte[] buf) {
-	        if (buf == null)
-	                return "";
-	        StringBuffer result = new StringBuffer(2*buf.length);
-	        for (int i = 0; i < buf.length; i++) {
-	                appendHex(result, buf[i]);
-	        }
-	        return result.toString();
-	}
-	private final static String HEX = "0123456789abcdef";
-	private static void appendHex(StringBuffer sb, byte b) {
-	        sb.append(HEX.charAt((b>>4)&0x0f)).append(HEX.charAt(b&0x0f));
 	}
 }
